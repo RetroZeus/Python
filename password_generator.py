a58d9cbab160638abfc0c06e08a90f74e3116ea5
Tejus C <88100167+RetroZeus@users.noreply.github.com>
2024-06-23 12:31:24 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,19 +1,17 @@
 import string
-from random import *
-
-UPPER_ALPHABETS = string.ascii_uppercase # all upper alphabets (A-Z)
-lower_alphabets = string.ascii_lowercase # all lower alphabets (a-z)
-numbers = string.digits # all digits (0-9)
-
-passlist = [] 
-passlist.extend(UPPER_ALPHABETS)              
-passlist.extend(lower_alphabets)
-passlist.extend(numbers)
-
-shuffle(passlist)
+from random import shuffle
 
 def create_password(length):
-    global passlist
+    UPPER_ALPHABETS = string.ascii_uppercase # all upper alphabets (A-Z)
+    lower_alphabets = string.ascii_lowercase # all lower alphabets (a-z)
+    numbers = string.digits # all digits (0-9)
+
+    passlist = [] 
+    passlist.extend(UPPER_ALPHABETS)              
+    passlist.extend(lower_alphabets)
+    passlist.extend(numbers)
+
+    shuffle(passlist)
     password = ''.join(passlist[0:length])
     return password
 
